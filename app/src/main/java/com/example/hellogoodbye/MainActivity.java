3596f176e4b749063b2e3ecd1c0c037a5c2c62b5
Doug179 <pizzaken79@hotmail.com>
2022-09-04 21:04:09 -0500

    Attempting button event 2

--- app/src/main/java/com/example/hellogoodbye/MainActivity.java
+++ app/src/main/java/com/example/hellogoodbye/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hellogoodbye;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.clickme_btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View Button) {
+                // Do something in response to button click
+            }
+        });
 
 
     }
